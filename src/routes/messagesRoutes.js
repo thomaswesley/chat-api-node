534a0b441c804bedc160402fd6450d1915b54985
thomaswesley <bragatrabalho@gmail.com>
2025-06-26 06:34:54 -0300

    chore(deploy): ajusta configurações para funcionamento do app.ts no Railway

--- src/routes/messagesRoutes.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import MessagesController from '../controllers/MessagesController.js';
-const router = express.Router();
-router.get('/messages', MessagesController.getMessages);
-router.post('/messages', MessagesController.postMessage);
-export default router;
