f5ea9525e8afeaa0186ac48062ab2eaae9a4adfb
thomaswesley <bragatrabalho@gmail.com>
2025-06-26 06:24:01 -0300

    chore(deploy): ajusta configurações para funcionamento do app.ts no Railway

---
+++ src/models/Messages.js
@@ -0,0 +1,35 @@
+import { connectDatabaseMySQL } from '../db/connectDatabaseMySQL.js';
+import dotenv from 'dotenv';
+dotenv.config();
+const saveMessage = async (message) => {
+    const now = new Date().toLocaleString('en-US', { timeZone: 'America/Sao_Paulo' });
+    const createdAt = new Date(now);
+    try {
+        const connection = await connectDatabaseMySQL();
+        const sql = 'INSERT INTO messages (sender, content, created_at) VALUES (?, ?, ?)';
+        const values = [message.sender, message.content, createdAt];
+        const [result] = await connection.execute(sql, values);
+        return result;
+    }
+    catch (error) {
+        console.error('Erro ao salvar mensagem:', error);
+        throw error;
+    }
+};
+const getMessages = async () => {
+    try {
+        const connection = await connectDatabaseMySQL();
+        const sql = 'SELECT sender, content, created_at FROM messages ORDER BY created_at ASC';
+        const [rows] = await connection.execute(sql);
+        return rows;
+    }
+    catch (error) {
+        console.error('Erro ao buscar mensagens:', error);
+        throw error;
+    }
+};
+const Messages = {
+    saveMessage,
+    getMessages,
+};
+export default Messages;
